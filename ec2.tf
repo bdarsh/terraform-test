64a8ba5be7d57c26af57e16fd008680f107c0157
Darshan <70128281+DarshanBhoi@users.noreply.github.com>
2020-09-27 11:02:05 -0400

    Update ec2.tf
    Added tags to assign a name to the instance.

--- ec2.tf
+++ ec2.tf
@@ -2,4 +2,7 @@
 resourse "aws_instance" "terraform-cloud-test-ec2"{
   ami = "ami-0947d2ba12ee1ff75"                   # Amazon Linux 2 AMI (HVM), SSD Volume Type - AWS Free Tire Eligible. AMI ID depends on the regions.  
   instance_type = "t2.micro"                      # AWS Free Tire Eligible.
+  tags = {
+    "Name" =  "Terraform-Cloud-Test"
+    }
 }
