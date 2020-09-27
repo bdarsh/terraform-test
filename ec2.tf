a8cc6845bb2cf18959984846023ba89d37d1f427
Darshan <70128281+DarshanBhoi@users.noreply.github.com>
2020-09-27 10:59:55 -0400

    Create ec2.tf
    For testing created .tf file.

---
+++ ec2.tf
@@ -0,0 +1,5 @@
+# Creating an EC2 instance via Terraform Cloud.
+resourse "aws_instance" "terraform-cloud-test-ec2"{
+  ami = "ami-0947d2ba12ee1ff75"                   # Amazon Linux 2 AMI (HVM), SSD Volume Type - AWS Free Tire Eligible. AMI ID depends on the regions.  
+  instance_type = "t2.micro"                      # AWS Free Tire Eligible.
+}
