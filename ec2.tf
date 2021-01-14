37fecebb7f9069fc388986c864f9703a709f5d7e
Darshan <70128281+bdarsh@users.noreply.github.com>
2021-01-14 00:43:11 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,5 @@
+#
+
 # Creating an EC2 instance via Terraform Cloud.
 resource "aws_instance" "terraform-cloud-test-ec2"{
   ami = "ami-0947d2ba12ee1ff75"                   # Amazon Linux 2 AMI (HVM), SSD Volume Type - AWS Free Tire Eligible. AMI ID depends on the regions.  
